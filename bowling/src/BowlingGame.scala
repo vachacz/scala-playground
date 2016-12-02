21056374c60614f51d2544a24c5285204a5c0f79
Wachowicz,  Łukasz <lukasz.wachowicz@mobica.com>
2016-12-02 15:24:52 +0100

    Final tweaks

--- bowling/src/BowlingGame.scala
+++ bowling/src/BowlingGame.scala
@@ -13,10 +13,6 @@ class BowlingGame {
     var first : Option[Int] = None
     var second : Option[Int] = None
 
-    def isSpare = second.isDefined && (first.get + second.get == 10)
-    def isStrike = first.isDefined && first.get == 10
-    def isComplete = isStrike || second.isDefined
-
     def roll(pins : Int) = {
       val firstRoll = first.isEmpty
       if (firstRoll) first = Some(pins)
@@ -33,12 +29,16 @@ class BowlingGame {
     }
 
     def score = bonus + List(first, second).filter(_.isDefined).map(_.get).sum
+
+    def isSpare = second.isDefined && (first.get + second.get == 10)
+    def isStrike = first.isDefined && first.get == 10
+    def isComplete = isStrike || second.isDefined
   }
 
-  var frames : List[Frame] = List()
+  var frames : List[Frame] = List(new FirstFrame())
 
   def roll(pins : Int) = {
-    if (frames.isEmpty || currentFrame.isComplete) addFrame
+    if (currentFrame.isComplete) addFrame
     currentFrame.roll(pins)
   }
 
@@ -47,7 +47,6 @@ class BowlingGame {
   private def currentFrame = frames.head
   private def addFrame = {
     frames = (frames.size match {
-      case s if s == 0 => new FirstFrame()
       case s if s < 9 => new Frame(Some(currentFrame))
       case _ => new LastFrame(Some(currentFrame))
     }) :: frames
@@ -56,43 +55,39 @@ class BowlingGame {
 
 object BowlingGameTest {
 
-  def test(rolls: List[Int], score: List[Int]) = {
-    val game2 = new BowlingGame()
-    (rolls zip score).foreach { e =>
-      game2.roll(e._1)
-      assert(e._2 == game2.score())
-    }
-  }
-
   def main(args: Array[String]): Unit = {
 
-    //              1         2    3         4         5    6    7     8           9                10
+    // FRAME        1         2    3         4         5    6    7     8           9                10
     val rolls = 4 ~ 4 I  4 ~  4 I 10 I  2 ~  8 I  5 ~  2 I 10 I 10 I  10 I   1 ~   1 I  10 ~   6 ~   3
     val score = 4 ~ 8 I 12 ~ 16 I 26 I 30 ~ 46 I 56 ~ 58 I 68 I 88 I 118 I 121 ~ 123 I 133 ~ 139 ~ 142
 
     test(rolls.list, score.list)
 
-    //            1    2    3    4     5     6     7     8     9                10
+    // FRAME      1    2    3    4     5     6     7     8     9                10
     val rolls2 = 10 I 10 I 10 I 10 I  10 I  10 I  10 I  10 I  10 I  10 ~  10 ~  10
     val score2 = 10 I 30 I 60 I 90 I 120 I 150 I 180 I 210 I 240 I 270 ~ 290 ~ 300
 
     test(rolls2.list, score2.list)
 
-    //              1       2       3       4       5       6       7       8       9      10
+    // FRAME         1       2       3       4       5       6       7       8       9      10
     val rolls3 = 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0
     val score3 = 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0 I 0 ~ 0
 
     test(rolls3.list, score3.list)
   }
 
-  implicit def intToListBuilder(e: Int) : ListBuilder = new ListBuilder(List(e))
+  def test(rolls: List[Int], score: List[Int]) = {
+    val game = new BowlingGame()
+    (rolls zip score).foreach { e =>
+      game.roll(e._1)
+      assert(e._2 == game.score())
+    }
+  }
 
-  def game(e: Int) = new ListBuilder(List(e))
+  implicit def intToListBuilder(e: Int) : ListBuilder = new ListBuilder(List(e))
 
   class ListBuilder(var list : List[Int] = List()) {
     def ~(e : Int) = new ListBuilder(list ::: List(e))
-    def I(e : Int) = this.~(e)
-    def ~(e : ListBuilder) = new ListBuilder(list ::: e.list)
-    def I(e : ListBuilder) = this.~(e)
+    def I(e : ListBuilder) = new ListBuilder(list ::: e.list)
   }
 }
