a9a8998b2a3e1b9968a18df739b069b22f4ba5cd
Wachowicz,  Łukasz <lukasz.wachowicz@mobica.com>
2016-12-05 16:50:35 +0100

    tail recursion version optimised

--- bowling/src/main/scala/BowlingGameTailRecursion.scala
+++ bowling/src/main/scala/BowlingGameTailRecursion.scala
@@ -4,18 +4,20 @@ class BowlingGameTailRecursion {
   var rolls: List[Int] = List()
 
   def roll(pins : Int) = rolls = rolls :+ pins
-  def score = scoreRecursive(0, rolls)
+  def score = scoreRecursive(0, 1, rolls)
 
-  def scoreRecursive(score: Int, rolls: List[Int]) : Int = {
-    rolls match{
-      case first :: remainingRolls if first == 10
-        => scoreRecursive(score + 10 + remainingRolls.take(2).sum, remainingRolls)
-      case first :: second :: remainingRolls if first + second == 10
-        => scoreRecursive(score + 10 + remainingRolls.take(1).sum, remainingRolls)
-      case first :: second :: remainingRolls
-        => scoreRecursive(score + first + second, remainingRolls)
-      case first :: remainingRolls
-        => scoreRecursive(score + first, remainingRolls)
+  def scoreRecursive(score: Int, frame: Int, rolls: List[Int]) : Int = {
+    (frame, rolls) match {
+      case (11, _)
+        => score
+      case (_, first :: remainingRolls) if first == 10
+        => scoreRecursive(score + 10 + remainingRolls.take(2).sum, frame + 1, remainingRolls)
+      case (_, first :: second :: remainingRolls) if first + second == 10
+        => scoreRecursive(score + 10 + remainingRolls.head, frame + 1, remainingRolls)
+      case (_, first :: second :: remainingRolls)
+        => scoreRecursive(score + first + second, frame + 1, remainingRolls)
+      case (_, first :: remainingRolls)
+        => scoreRecursive(score + first, frame + 1, remainingRolls)
       case _ => score
     }
   }
