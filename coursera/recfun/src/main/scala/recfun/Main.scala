da3bcb199286a7892bd8eabd6ce2747c3df4f13d
Wachowicz,  Łukasz <lukasz.wachowicz@mobica.com>
2016-12-08 16:22:10 +0100

    excercise 1 and 3

--- coursera/recfun/src/main/scala/recfun/Main.scala
+++ coursera/recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,32 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = (c, r) match {
+    case (0, _) => 1
+    case (x, y) if x == y => 1
+    case (x, y) => pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money == 0) return 0
+
+    def _count(money: Int, coins: List[Int]): Int = (money, coins) match {
+      case (0, _) => 1
+      case (_, Nil) => 0
+      case (m, coin :: tail) if m >= coin => _count(m - coin, coins) + _count(m, tail)
+      case _ => 0
+    }
+
+    _count(money, coins.sortWith(_ < _))
+  }
+}
